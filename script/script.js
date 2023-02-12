02f61f601fa8f0b7fef4cfcebd814ba57aaf55dd
rozexcode <patrykrozpedek02@gmail.com>
2023-02-12 12:25:54 +0100

    bootstrap testing on course

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// const navElements = document.querySelector('.nav-li-container')
+
+
+// navElements.addEventListener('mouseover', function(e) {
+    
+//     // console.log(e)
+//     if(e.target.classList.contains('nav-li'))
+//         e.target.classList.add('selected')
+// })
+
+// navElements.addEventListener('mouseout', function(e) {
+//     if(e.target.classList.contains('nav-li'))
+//         e.target.classList.remove('selected')
+            
+// })
